869891be7cb46fde7617c8e8a245438e9537b763
Parvinder Singh <psratanpal@gmail.com>
2022-10-17 20:57:41 +0530

    fix gor app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 
 const app = express();
 
-const path = process.env.PORT
+const port = process.env.PORT
 
 //define paths for hbs config
 const publicDirPath = path.join(__dirname,'../public')
